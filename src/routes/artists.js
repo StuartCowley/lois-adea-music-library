754ca4d18751fae8d552a31b451e79f1688cd949
Lois Adea <loisadea@gmail.com>
2023-07-16 23:01:27 +0100

    Implement CRUD methods

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const { createArtist } = require("../controllers/artists");
+const {
+  readArtists,
+  readArtist,
+  createArtist,
+  updateArtist,
+  deleteArtist,
+} = require("../controllers/artists");
 
 const router = express.Router();
 router.post("/", createArtist);
+router.get("/", readArtists);
+router.get("/:id", readArtist);
+router.put("/:id", updateArtist);
+router.delete("/:id", deleteArtist);
 module.exports = router;
