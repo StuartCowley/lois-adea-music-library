0ceab6047d382dda96b9de89422d3f3ae1f5c187
Lois Adea <loisadea@gmail.com>
2023-07-16 01:02:23 +0100

    Test setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
-});
+app.use("/artists", artists);
 
 module.exports = app;
